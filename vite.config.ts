4110133a4d7d039a03facf9e5836088cd8dc9941
Jesse Gibson <JesseTheGibson@gmail.com>
2023-12-22 13:18:49 -0500

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import * as fs from 'node:fs/promises';
+import { builtinModules } from 'node:module';
+import * as path from 'node:path';
+
+import { defineConfig } from 'vite';
+
+export default async () => {
+  const pkg = JSON.parse(await fs.readFile('./package.json', 'utf8'));
+  const natives = builtinModules.map((mod) => `node:${mod}`);
+  const deps = Object.keys(pkg.dependencies ?? {});
+
+  return defineConfig({
+    build: {
+      target: 'esnext',
+      lib: {
+        entry: path.join(__dirname, './src/index.ts'),
+        fileName: 'minilang',
+        formats: ['es'],
+      },
+      rollupOptions: {
+        external: builtinModules.concat(natives).concat(deps),
+      },
+    },
+  });
+};
