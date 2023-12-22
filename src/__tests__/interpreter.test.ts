f6ec7012abec664bb644e82925974d65271bef42
Jesse Gibson <JesseTheGibson@gmail.com>
2023-12-22 16:18:54 -0500

    Add support for binding identifiers
    Identifiers can be bound to any value. They can be bound to other
    identifiers and inherit (or override) parent scopes.

--- src/__tests__/interpreter.test.ts
+++ src/__tests__/interpreter.test.ts
@@ -1,4 +1,4 @@
-import { str, num, bool } from '../builders';
+import { str, num, bool, bind, assign, ident } from '../builders';
 import { interpret } from '../interpreter';
 import { compile } from '../compiler';
 import type * as T from '../values';
@@ -40,6 +40,54 @@ describe('interpreter', () => {
     expect(run(bool(false))).toEqual<T.BooleanValue>({
       type: ValueType.Boolean,
       value: false,
+    });
+  });
+
+  it('can resolve identifiers where the value is in scope', () => {
+    const program = bind(
+      [assign(ident('msg'), str('hello world'))],
+      ident('msg'),
+    );
+
+    expect(run(program)).toEqual<T.StringValue>({
+      type: ValueType.String,
+      value: 'hello world',
+    });
+  });
+
+  it('can resolve identifiers from an outer scope', () => {
+    const program = bind(
+      [assign(ident('x'), num(1))],
+      bind([assign(ident('y'), num(2))], ident('x')),
+    );
+
+    expect(run(program)).toEqual<T.NumberValue>({
+      type: ValueType.Number,
+      value: 1,
+    });
+  });
+
+  it('can resolve identifiers defined in the same binding', () => {
+    const program = bind(
+      [assign(ident('x'), num(1)), assign(ident('y'), ident('x'))],
+      ident('y'),
+    );
+
+    expect(run(program)).toEqual<T.NumberValue>({
+      type: ValueType.Number,
+      value: 1,
+    });
+  });
+
+  it('can override identifiers from the outer scope', () => {
+    const program = bind(
+      [assign(ident('x'), num(1))],
+      bind([assign(ident('x'), num(2))], ident('x')),
+    );
+
+    expect(run(program)).toEqual<T.NumberValue>({
+      type: ValueType.Number,
+      value: 2,
     });
   });
 });
--- src/interpreter.ts
+++ src/interpreter.ts
@@ -7,11 +7,13 @@ import { ValueType } from './values';
 export function interpret(program: CompilerOutput): InterpreterOutput {
   return {
     warnings: program.warnings,
-    value: run(program.program),
+    value: run(program.program, {
+      environment: new Map(),
+    }),
   };
 }
 
-function run(expression: Expression): T.Value {
+function run(expression: Expression, context: EvaluationContext): T.Value {
   switch (expression.type) {
     case NodeType.StringLiteral: {
       return {
@@ -34,8 +36,37 @@ function run(expression: Expression): T.Value {
       };
     }
 
+    case NodeType.BindExpression: {
+      // Evaluate the bindings in order, adding them to the environment.
+      const contextWithBindings = expression.bindings.reduce(
+        (layeredContext, binding) => ({
+          ...layeredContext,
+          environment: new Map([
+            ...layeredContext.environment,
+            [binding.identifier.name, run(binding.value, layeredContext)],
+          ]),
+        }),
+        context,
+      );
+
+      return run(expression.body, contextWithBindings);
+    }
+
+    case NodeType.Identifier: {
+      const value = context.environment.get(expression.name);
+
+      // This should be caught by the compiler before it can get here.
+      if (value === undefined) {
+        throw new InternalError(
+          `Could not resolve identifier "${expression.name}"`,
+        );
+      }
+
+      return value;
+    }
+
     default: {
-      throw new Error(`Unexpected node type: ${expression.type}`);
+      throw new InternalError(`Unexpected node type: ${expression.type}`);
     }
   }
 }
@@ -43,4 +74,14 @@ function run(expression: Expression): T.Value {
 interface InterpreterOutput {
   warnings: Array<CompilerMessage>;
   value: T.Value;
+}
+
+interface EvaluationContext {
+  /** Any variables in scope. */
+  environment: Map<string, T.Value>;
+}
+
+/** These indicate a language bug. */
+class InternalError extends Error {
+  name = 'InternalError';
 }
